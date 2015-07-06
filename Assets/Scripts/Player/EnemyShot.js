4a2833dcdfaa5b22e27918996fd05bffa8ad3b49
pprado <pprado@thoughtworks.com>
2015-07-06 00:08:10 -0300

    Player pode matar e ser morto pelo inimigo.

--- Assets/Scripts/Player/EnemyShot.js
+++ Assets/Scripts/Player/EnemyShot.js
@@ -7,32 +7,36 @@ var numberOfShotsReceived : float = 0;
 private var tankRunning : AudioSource;
 private var tankShot : AudioSource;
 var windowRect : Rect = Rect (20, 20, 120, 50);
-
+var nextUsage : float = 0;
+var delay=2;//two seconds delay.
+ 
 function Start() {
-	
-	var sounds = gameObject.GetComponents(AudioSource);
- 	tankRunning = sounds[0];
-	tankShot = sounds[1];
+    nextUsage = Time.time + delay;
+    var sounds = gameObject.GetComponents(AudioSource);
+     tankRunning = sounds[0];
+    tankShot = sounds[1];
 }
 
 function Update () {
 
-   	// Fire!
-	//Instantiate(fireEffect, spawnPoint.position, spawnPoint.rotation);
-	// make ball
-	//Instantiate(bulletObject, spawnPoint.position, spawnPoint.rotation);
-	
-	//tankShot.Play();
-	
+    if (Time.time > nextUsage) {
+        nextUsage = Time.time + delay;
+           // Fire!
+        Instantiate(fireEffect, spawnPoint.position, spawnPoint.rotation);
+        // make ball
+        Instantiate(bulletObject, spawnPoint.position, spawnPoint.rotation);
+        
+        tankShot.Play();
+    }
+    
 }
 
 //Shot received
 function OnTriggerEnter(hit: Collider) {
 
-	numberOfShotsReceived += 1;
-	if(numberOfShotsReceived == 3) {
-		Destroy(gameObject);
-		Application.LoadLevel("EndGame");
-	}
+    numberOfShotsReceived += 1;
+    if(numberOfShotsReceived == 5) {
+        Destroy(gameObject);
+        Application.LoadLevel("End Game");
+    }
 }
-
--- Assets/Scripts/Player/MoveTank.js
+++ Assets/Scripts/Player/MoveTank.js
@@ -26,132 +26,140 @@ private var tankShot : AudioSource;
 
 
 function Start() {
-	// Get Track Controls
-	leftTrack = GameObject.Find(gameObject.name + "/Lefttrack").GetComponent(MoveTrack);
-	rightTrack = GameObject.Find(gameObject.name + "/Righttrack").GetComponent(MoveTrack);
-	
-	var sounds = gameObject.GetComponents(AudioSource);
- 	tankRunning = sounds[0];
-	tankShot = sounds[1];
-	reloadBullet = 0;
+    // Get Track Controls
+    leftTrack = GameObject.Find(gameObject.name + "/Lefttrack").GetComponent(MoveTrack);
+    rightTrack = GameObject.Find(gameObject.name + "/Righttrack").GetComponent(MoveTrack);
+    
+    var sounds = gameObject.GetComponents(AudioSource);
+     tankRunning = sounds[0];
+    tankShot = sounds[1];
+    reloadBullet = 0;
 }
 
 function Update () {
-	
-	if (Input.GetKey (KeyCode.UpArrow)) {
-		// plus speed
-		if (currentVelocity <= maxSpeed) 
-			currentVelocity += acceleration * Time.deltaTime;
+    
+    if (Input.GetKey (KeyCode.UpArrow)) {
+        // plus speed
+        if (currentVelocity <= maxSpeed) 
+            currentVelocity += acceleration * Time.deltaTime;
 
-	} else if (Input.GetKey (KeyCode.DownArrow)) {
-		// minus speed
-		if (currentVelocity >= -maxSpeed) 
-			currentVelocity -= acceleration * Time.deltaTime;
-		
-	} else {
-		// No key input. 
-		if (currentVelocity > 0) 
-			currentVelocity -= acceleration * Time.deltaTime;
-		else if (currentVelocity < 0) 
-			currentVelocity += acceleration * Time.deltaTime;
-	}
-	
-	tankRunning.volume = (Mathf.Abs(currentVelocity) / maxSpeed) + 0.01;
+    } else if (Input.GetKey (KeyCode.DownArrow)) {
+        // minus speed
+        if (currentVelocity >= -maxSpeed) 
+            currentVelocity -= acceleration * Time.deltaTime;
+        
+    } else {
+        // No key input. 
+        if (currentVelocity > 0) 
+            currentVelocity -= acceleration * Time.deltaTime;
+        else if (currentVelocity < 0) 
+            currentVelocity += acceleration * Time.deltaTime;
+    }
+    
+    tankRunning.volume = (Mathf.Abs(currentVelocity) / maxSpeed) + 0.01;
 
-	// Turn off engine if currentVelocity is too small. 
-	if (Mathf.Abs(currentVelocity) <= 0.05) {
-		currentVelocity = 0;
-	}	
+    // Turn off engine if currentVelocity is too small. 
+    if (Mathf.Abs(currentVelocity) <= 0.05) {
+        currentVelocity = 0;
+    }    
 
-	// Move Tank by currentVelocity
-	transform.Translate(Vector3(0, 0, currentVelocity * Time.deltaTime));
+    // Move Tank by currentVelocity
+    transform.Translate(Vector3(0, 0, currentVelocity * Time.deltaTime));
 
-	// Move Tracks by currentVelocity	 
-	if (currentVelocity > 0) {
-		// Move forward
-		leftTrack.speed = currentVelocity;
-		leftTrack.GearStatus = 1;
-		rightTrack.speed = currentVelocity;
-		rightTrack.GearStatus = 1;
-	}
-	else if (currentVelocity < 0)	{
-		// Move Backward
-		leftTrack.speed = -currentVelocity;
-		leftTrack.GearStatus = 2;
-		rightTrack.speed = -currentVelocity;
-		rightTrack.GearStatus = 2;
-	}
-	else {
-		// No Move
-		leftTrack.GearStatus = 0;	
-		rightTrack.GearStatus = 0;		
-	}
+    // Move Tracks by currentVelocity     
+    if (currentVelocity > 0) {
+        // Move forward
+        leftTrack.speed = currentVelocity;
+        leftTrack.GearStatus = 1;
+        rightTrack.speed = currentVelocity;
+        rightTrack.GearStatus = 1;
+    }
+    else if (currentVelocity < 0)    {
+        // Move Backward
+        leftTrack.speed = -currentVelocity;
+        leftTrack.GearStatus = 2;
+        rightTrack.speed = -currentVelocity;
+        rightTrack.GearStatus = 2;
+    }
+    else {
+        // No Move
+        leftTrack.GearStatus = 0;    
+        rightTrack.GearStatus = 0;        
+    }
 
 
-	// Turn Tank
-	if (Input.GetKey (KeyCode.LeftArrow)) {
-		if (Input.GetKey(KeyCode.DownArrow)) {
-			// Turn right
-			transform.Rotate(Vector3(0, rotationSpeed * Time.deltaTime, 0));
-			
-			leftTrack.speed = rotationSpeed;
-			leftTrack.GearStatus = 1;
-			rightTrack.speed = rotationSpeed;
-			rightTrack.GearStatus = 2;
-			
-		} else {
-			// Turn left
-			transform.Rotate(Vector3(0, -rotationSpeed * Time.deltaTime, 0));
-			
-			leftTrack.speed = rotationSpeed;
-			leftTrack.GearStatus = 2;
-			rightTrack.speed = rotationSpeed;
-			rightTrack.GearStatus = 1;
-			
-		}
-	}
+    // Turn Tank
+    if (Input.GetKey (KeyCode.LeftArrow)) {
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            // Turn right
+            transform.Rotate(Vector3(0, rotationSpeed * Time.deltaTime, 0));
+            
+            leftTrack.speed = rotationSpeed;
+            leftTrack.GearStatus = 1;
+            rightTrack.speed = rotationSpeed;
+            rightTrack.GearStatus = 2;
+            
+        } else {
+            // Turn left
+            transform.Rotate(Vector3(0, -rotationSpeed * Time.deltaTime, 0));
+            
+            leftTrack.speed = rotationSpeed;
+            leftTrack.GearStatus = 2;
+            rightTrack.speed = rotationSpeed;
+            rightTrack.GearStatus = 1;
+            
+        }
+    }
 
-	if (Input.GetKey (KeyCode.RightArrow)) {
-		if (Input.GetKey(KeyCode.DownArrow)) {
-			// Turn left
-			transform.Rotate(Vector3(0, -rotationSpeed * Time.deltaTime, 0));
-			leftTrack.speed = rotationSpeed;
-			leftTrack.GearStatus = 2;
-			rightTrack.speed = rotationSpeed;
-			rightTrack.GearStatus = 1;
+    if (Input.GetKey (KeyCode.RightArrow)) {
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            // Turn left
+            transform.Rotate(Vector3(0, -rotationSpeed * Time.deltaTime, 0));
+            leftTrack.speed = rotationSpeed;
+            leftTrack.GearStatus = 2;
+            rightTrack.speed = rotationSpeed;
+            rightTrack.GearStatus = 1;
 
-		} else {
-			// Turn right
-			transform.Rotate(Vector3(0, rotationSpeed * Time.deltaTime, 0));
-			leftTrack.speed = rotationSpeed;
-			leftTrack.GearStatus = 1;
-			rightTrack.speed = rotationSpeed;
-			rightTrack.GearStatus = 2;
-			
-		}
-	}
-	
-	
-	// Fire!
-	if (Input.GetButtonDown("Fire1") && remainingBullets > 0 && reloadBullet == 0) {
-		// make fire effect.
-		Instantiate(fireEffect, spawnPoint.position, spawnPoint.rotation);
-		
-		// make ball
-		Instantiate(bulletObject, spawnPoint.position, spawnPoint.rotation);
-		
-		tankShot.Play();
-		
-		reloadBullet = 1;
-		remainingBullets--;
-		
-		StartCoroutine("ReloadBulletThread");
-	}
-	
+        } else {
+            // Turn right
+            transform.Rotate(Vector3(0, rotationSpeed * Time.deltaTime, 0));
+            leftTrack.speed = rotationSpeed;
+            leftTrack.GearStatus = 1;
+            rightTrack.speed = rotationSpeed;
+            rightTrack.GearStatus = 2;
+            
+        }
+    }
+    
+    
+    // Fire!
+    if (Input.GetButtonDown("Fire1") && remainingBullets > 0 && reloadBullet == 0) {
+        // make fire effect.
+        Instantiate(fireEffect, spawnPoint.position, spawnPoint.rotation);
+        
+        // make ball
+        Instantiate(bulletObject, spawnPoint.position, spawnPoint.rotation);
+        
+        tankShot.Play();
+        
+        reloadBullet = 1;
+        remainingBullets--;
+        
+        StartCoroutine("ReloadBulletThread");
+    }
+    
+}
+
+//Shot received
+function OnTriggerEnter(hit: Collider) {
+
+    numberOfShotsReceived += 1;
+    if(numberOfShotsReceived == 2) {
+        Application.LoadLevel("End Game");
+    }
 }
 
 function ReloadBulletThread() {
-	yield WaitForSeconds(3);
-	reloadBullet = 0;
+    yield WaitForSeconds(3);
+    reloadBullet = 0;
 }
-
